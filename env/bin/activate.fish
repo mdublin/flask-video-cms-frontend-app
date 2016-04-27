0b0e7bb8a24eae0d838e12bd43579a930c98338d
mdublin <alegionofgenius@gmail.com>
2016-04-27 03:16:31 -0400

    updated env and included actual local requirements.txt

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/mdublin/Downloads/newapp/env"
+set -gx VIRTUAL_ENV "/Users/mdublin/Downloads/flask-video-cms-frontend-app/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
