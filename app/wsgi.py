c4cd531f0f2cc6a3d0c139ebf714596cb2e2a393
mdublin <alegionofgenius@gmail.com>
2016-05-01 17:23:01 -0400

    added Gunicorn, start server with $ gunicorn app.wsgi:application --log-level debug

---
+++ app/wsgi.py
@@ -0,0 +1,13 @@
+from whitenoise import WhiteNoise
+import os
+
+config_name = os.environ.get('FLASK_CONFIG') or 'production'
+
+from app import create_app
+
+application = create_app(config_name)
+
+#application = app()
+application = WhiteNoise(application, root='/path/to/static/files')
+application.add_files('/path/to/more/static/files', prefix='more-files/')
+
