ac0e03dbe075bbe8cf1cf293f368f9a948afc74f
mdublin <alegionofgenius@gmail.com>
2016-05-01 20:03:11 -0400

    migrating to Postgres

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: gunicorn app.wsgi:application --log-file -
-
--- config/production.py
+++ config/production.py
@@ -3,5 +3,6 @@ import os
 ADMINS = ['admin@test.com']
 DEBUG = False
 SECRET_KEY = 'top secret!'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(
-    os.path.dirname(__file__), '../data.sqlite3')
+SQLALCHEMY_DATABASE_URI = 'postgresql://username:password@localhost:5432/VideoDashBoard'
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(
+#    os.path.dirname(__file__), '../data.sqlite3')
