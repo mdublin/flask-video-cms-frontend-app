ab012c1824395fc0183229e5bb5d8536de21dfb8
mdublin <alegionofgenius@gmail.com>
2016-05-01 22:24:59 -0400

    trying to fix heroku postgres

--- config/production.py
+++ config/production.py
@@ -10,8 +10,10 @@ SECRET_KEY = 'top secret!'
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(
 #    os.path.dirname(__file__), '../data.sqlite3')
 
+DATABASE_URL = "postgres://oxogvaquetjage:X4wK3auttNuB2oZTkqzoI7Gid_@ec2-54-243-196-42.compute-1.amazonaws.com:5432/df6vtpcae81mm5"
 urlparse.uses_netloc.append("postgres")
-url = urlparse.urlparse(os.environ["DATABASE_URL"])
+url = urlparse.urlparse(os.environ[DATABASE_URL])
+#url = urlparse.urlparse(os.environ["DATABASE_URL"])
 #url = urlparse.urlparse(os.environ["postgres://oxogvaquetjage:X4wK3auttNuB2oZTkqzoI7Gid_@ec2-54-243-196-42.compute-1.amazonaws.com:5432/df6vtpcae81mm5"])
 conn = psycopg2.connect(
     database=url.path[1:],
