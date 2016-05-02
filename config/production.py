6c7cc1dd6c7f39ba0a46a152eeaba49d722bd7d5
mdublin <alegionofgenius@gmail.com>
2016-05-01 21:06:19 -0400

    added db URL to production.py

--- config/production.py
+++ config/production.py
@@ -11,7 +11,7 @@ SECRET_KEY = 'top secret!'
 #    os.path.dirname(__file__), '../data.sqlite3')
 
 urlparse.uses_netloc.append("postgres")
-url = urlparse.urlparse(os.environ["DATABASE_URL"])
+url = urlparse.urlparse(os.environ["postgres://oxogvaquetjage:X4wK3auttNuB2oZTkqzoI7Gid_@ec2-54-243-196-42.compute-1.amazonaws.com:5432/df6vtpcae81mm5"])
 conn = psycopg2.connect(
     database=url.path[1:],
     user=url.username,
