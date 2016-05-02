4c3df926b7f9a2536c7fef254e81b65d8c9e9a26
mdublin <alegionofgenius@gmail.com>
2016-05-01 20:57:11 -0400

    Amended production.py with PostgreSQL guidelines here https://devcenter.heroku.com/articles/heroku-postgresql#connecting-in-python

--- config/production.py
+++ config/production.py
@@ -1,8 +1,22 @@
 import os
 
+import psycopg2
+import urlparse
+
 ADMINS = ['admin@test.com']
 DEBUG = False
 SECRET_KEY = 'top secret!'
-SQLALCHEMY_DATABASE_URI = 'postgresql://username:password@localhost:5432/VideoDashBoard'
+#SQLALCHEMY_DATABASE_URI = 'postgresql://username:password@localhost:5432/VideoDashBoard'
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(
 #    os.path.dirname(__file__), '../data.sqlite3')
+
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
+
