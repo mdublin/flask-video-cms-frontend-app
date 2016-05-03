8011ca88b9c8b9f750b7a50e3a60661bb7ab1067
mdublin <alegionofgenius@gmail.com>
2016-05-02 23:45:38 -0400

    trying to configure heroku postgres connection

--- config/production.py
+++ config/production.py
@@ -10,3 +10,18 @@ SECRET_KEY = 'top secret!'
 SQLALCHEMY_DATABASE_URI = "postgresql://mdublin1@localhost:5432/VideoDashBoard"
 
 
+import psycopg2
+import urlparse
+
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+conn = pyscopg2.connect(
+        database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        port=url.port
+)
+
+
+
