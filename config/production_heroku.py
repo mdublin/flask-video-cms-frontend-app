225457fcb29839df34ccd6d0a8ff339b7f2f6646
mdublin <alegionofgenius@gmail.com>
2016-05-03 02:31:01 -0400

    safety

--- config/production_heroku.py
+++ config/production_heroku.py
@@ -9,9 +9,9 @@ SECRET_KEY = 'top secret!'
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(
 #    os.path.dirname(__file__), '../data.sqlite3')
 
-SQLALCHEMY_DATABASE_URI = "postgresql://mdublin1@localhost:5432/VideoDashBoard"
-#url = urlparse.urlparse(os.environ["DATABASE_URL"])
-#SQLALCHEMY_DATABASE_URI = url
+#SQLALCHEMY_DATABASE_URI = "postgresql://mdublin1@localhost:5432/VideoDashBoard"
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+SQLALCHEMY_DATABASE_URI = url
 
 
 '''import psycopg2
